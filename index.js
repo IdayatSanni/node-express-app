005099046a3048a240a74c56adbed80e69d7fd71
Idayat Sanni <idayatsanni@gmail.com>
2024-12-07 16:33:38 -0500

    allow request from all

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-//import required modules
+// Import required modules
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const cors = require("cors"); //for API paths to work when deployed
+const cors = require("cors"); // For API paths to work when deployed
 
-// page routes
+// Page routes
 const publicationRouter = require("./modules/publications/router");
 const menuRouter = require("./modules/menuLinks/router");
 const awardsRouter = require("./modules/awards/router");
@@ -14,10 +14,14 @@ const adminRouter = require("./modules/admin/router");
 const apiAwardRouter = require("./modules/awards/apiRouter");
 const apiPublicationRouter = require("./modules/publications/apiRouter");
 
-//set up Express object and port
+// Set up Express object and port
 const app = express();
 const port = process.env.PORT || "8888";
 
+// Set up CORS (allow all origins)
+app.use(cors());  // This allows requests from all origins
+
+// Set up route handlers
 app.use("/", publicationRouter);
 app.use("/", menuRouter);
 app.use("/", awardsRouter);
@@ -25,17 +29,13 @@ app.use("/", adminRouter);
 app.use("/", apiAwardRouter);
 app.use("/", apiPublicationRouter);
 
-//SET UP CORS TO ACCEPT REQUESTS FROM ANY ORIGIN
-app.use(cors());
-
-
-
+// Set up views and static files
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-//set up server listening
+// Set up server listening
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
